cd193d77ca07561ab740a8d0ed05911e8d53de09
Luiz Fernando <luizfernandossantos909@usp.br>
2022-10-31 22:53:23 -0300

    create post

--- src/app/dashboard/create-post/create-post.component.ts
+++ src/app/dashboard/create-post/create-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePostComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      assunto: [''],
+      texto: ['']
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  public publicar(){
+    
   }
 
 }
