b928da618fe240a4e456287ee1c043f9de8c4d95
CesarOuchi <ck137@hotmail.com>
2022-11-17 23:51:46 -0300

    Adjust URL (Temporary)

--- src/app/service/backend.service.ts
+++ src/app/service/backend.service.ts
@@ -14,9 +14,10 @@ import { Post } from '../model/post';
 export class BackendService {
 
   private readonly API_PESSOAS = '/assets/pessoas.json';
-  private readonly API_POST = 'http://localhost:3000/posts';
+  private readonly API_POST = 'https://my-party-db.herokuapp.com/posts';
+  //private readonly API_POST = 'http://localhost:3000/posts';
   //private readonly API_POST = '/assets/posts.json';
-  private readonly API_CADASTRO = '/assents/cadastro.json';
+  private readonly API_CADASTRO = '/assets/cadastro.json';
 
   LS_CHAVE: string = "usuarioLogado";
 
