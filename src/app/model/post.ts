ae2b08e5593fbf3efd6a9cf3f6bdeef5ea010f38
Henrique <henrytavares@usp.br>
2022-11-29 20:46:53 -0300

    initial changes

--- src/app/model/post.ts
+++ src/app/model/post.ts
@@ -1,9 +1,21 @@
 export class Post {
-    public id?: number;
+    public id?: string;
     public title?: string;
     public createdBy?: string;
     public language?: string;
     public description?: string;
     public categories?: string[];
     public likes?: number;
+
+    constructor(id: string, title: string, createdBy: string, language: string,
+        description: string, categories: string[], likes: number){
+
+       this.id = id;
+       this.title = title;
+       this.createdBy = createdBy;
+       this.language = language;
+       this.description = description;
+       this.categories = categories;
+       this.likes = likes;
+   }
 }
